a98a6674175f669b2d67fbe1b758035d28e1b1d2
TheoCRLDS <theo.carladous@laposte.net>
2023-07-05 10:08:06 +0200

    first commit

---
+++ database/migrations/2023_07_05_075322_create_comment_like_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('comment_like', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('liked')->default(false);
+            $table->bigInteger('comment_id');
+            $table->foreign('comment_id')->references('id')->on('comments');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment_like');
+    }
+};

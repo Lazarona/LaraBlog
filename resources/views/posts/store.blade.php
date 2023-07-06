956a7182f6a9b94531883cb26fee3b3418b92bb9
TheoCRLDS <theo.carladous@laposte.net>
2023-07-06 14:46:30 +0200

    affichage posts

--- resources/views/posts/store.blade.php
+++ resources/views/posts/store.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-        <div>
+<div class="d-flex flex-wrap gap-5">
             @foreach ($posts as $post)
-                <h1>{{$post->title}}</h1>
-                <p>{{$post->content}}</p>
+                <div class="card" style="width: 18rem;">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                    <div class="card-body">
+                      <h5 class="card-title">{{$post->title}}</h5>
+                      <p class="card-text">{{$post->content}}</p>
+                      <a href="{{route('posts.show', $post->id)}}" class="btn btn-primary">Comment</a>
+                </div>
+              </div>
             @endforeach
         </div>
 @endsection

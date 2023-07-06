5af7e4ec3dacf7ba5264f9870ee40bc5ef9df157
Celine CC <celine.c11@outlook.com>
2023-07-06 15:55:23 +0200

    page post

--- resources/views/posts/store.blade.php
+++ resources/views/posts/store.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-flex flex-wrap gap-5">
+<div class="d-flex flex-wrap gap-5 bg-dark ">
             @foreach ($posts as $post)
-                <div class="card" style="width: 18rem;">
+                <div class="card imageFond2 " style="width: 18rem;">
                 {{-- <img src="..." class="card-img-top" alt="..."> --}}
                     <div class="card-body">
-                      <h5 class="card-title">{{$post->title}}</h5>
+                      <h5 class="card-title fs-2 text fw-bold text-center">{{$post->title}}</h5>
                       <p class="card-text">{{$post->content}}</p>
-                      <a href="{{route('posts.show', $post->id)}}" class="btn btn-primary">Comment</a>
+                      <a href="{{route('posts.show', $post->id)}}" class="btn btn-primary justify-content-center">Comment</a>
                 </div>
+                
               </div>
+              
             @endforeach
         </div>
 @endsection

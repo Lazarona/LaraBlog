9036343975e5cf141b395e88b2b59728c42e1a67
TheoCRLDS <theo.carladous@laposte.net>
2023-07-06 16:12:20 +0200

    delete comment btn when not logged

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,9 @@
                     <div class="card-body">
                       <h5 class="card-title">{{$post->title}}</h5>
                       <p class="card-text">{{$post->content}}</p>
-                      <a href="{{route('posts.show', $post->id)}}" class="btn btn-secondary">Comment</a>
+                      @auth
+                      <a href="{{route('posts.show', $post->id)}}" class="btn btn-secondary">Comment</a> 
+                      @endauth
                 </div>
               </div>
             @endforeach
